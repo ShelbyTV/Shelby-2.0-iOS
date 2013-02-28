9a1d71563430a162b1a545ee5a69114f4ab75f2d
Keren Pinkas <keren@shelby.tv>
2013-02-28 10:25:10 -0500

    #159 - fix status bar display issue

--- Shelby.tv/Shelby.tv/SPVideoReel.m
+++ Shelby.tv/Shelby.tv/SPVideoReel.m
@@ -976,7 +976,6 @@
     [self.view bringSubviewToFront:self.screenshot];
     [self.view bringSubviewToFront:self.zoomInScreenshot];
     
-    [[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:UIStatusBarStyleBlackTranslucent];
     [UIView animateWithDuration:0.5 animations:^{
         [self.zoomInScreenshot setFrame:CGRectMake(-self.view.frame.size.width / 2, -self.view.frame.size.height / 2, self.view.frame.size.width * 2, self.view.frame.size.height * 2)];
     }];
@@ -1023,8 +1022,6 @@
     
     [self.view bringSubviewToFront:self.screenshot];
     [self.view bringSubviewToFront:self.zoomInScreenshot];
-    
-    [[UIApplication sharedApplication] setStatusBarHidden:NO withAnimation:UIStatusBarStyleBlackTranslucent];
     
     [self.overlayView.homeButton setHidden:YES];
     
