877160b69012f23222f6520a0dc3a06811cabfc0
Keren Pinkas <keren@shelby.tv>
2013-03-01 17:28:25 -0500

    #165 - setting the size in nib. First step in removing all references to sizes

--- Shelby.tv/Shelby.tv/BrowseViewController.m
+++ Shelby.tv/Shelby.tv/BrowseViewController.m
@@ -117,13 +117,8 @@ typedef enum {
     
     // If viewWillAppear is called when SPVideoReel modalVC is removed...
     if ( [[UIApplication sharedApplication] isStatusBarHidden] ) {
-        
         // ... re-display status bar
         [[UIApplication sharedApplication] setStatusBarHidden:NO withAnimation:UIStatusBarStyleBlackTranslucent];
-        
-        // ... and reset the view's frame
-        [self.view setFrame:CGRectMake(0.0f, 0.0f, 1024.0f, 748.0f)];
-        
     }
 
 }
