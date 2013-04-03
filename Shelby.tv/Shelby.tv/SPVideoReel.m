34be4fa66f6005478079b627f2824a8ec31071a0
Keren Pinkas <keren@shelby.tv>
2013-04-03 11:29:52 -0400

    Revert "#246 - add tap support for displaying share buttons"
    This reverts commit c4d36c25716dbc2129da677f0f4621b7ce91c069.
    
    Conflicts:
    	Shelby.tv/Shelby.tv/SPVideoReel.m

--- Shelby.tv/Shelby.tv/SPVideoReel.m
+++ Shelby.tv/Shelby.tv/SPVideoReel.m
@@ -14,7 +14,6 @@
 #import "SPVideoScrubber.h"
 #import "DeviceUtilities.h"
 #import "ImageUtilities.h"
-#import "ALRadialMenu.h"
 #import "GroupsMenuViewController.h"
 
 typedef NS_ENUM(NSUInteger, MenuState)
@@ -24,7 +23,7 @@ typedef NS_ENUM(NSUInteger, MenuState)
     MenuStateGroupsOpen,
 };
 
-@interface SPVideoReel () <ALRadialMenuDelegate>
+@interface SPVideoReel ()
 
 @property (weak, nonatomic) AppDelegate *appDelegate;
 @property (weak, nonatomic) SPModel *model;
@@ -38,11 +37,11 @@ typedef NS_ENUM(NSUInteger, MenuState)
 @property (copy, nonatomic) NSString *categoryID;
 @property (assign, nonatomic) BOOL fetchingOlderVideos;
 @property (assign, nonatomic) BOOL loadingOlderVideos;
-@property (nonatomic) ALRadialMenu *radialMenu;
 
 @property (strong, nonatomic) GroupsMenuViewController *groupsMenuViewController;
+
 @property (assign, nonatomic) MenuState menuState;
-@property (assign, nonatomic) BOOL radialMenuVisible;
+
 // Transition Properties
 @property (strong, nonatomic) UIImageView *screenshot;
 @property (strong, nonatomic) UIImageView *zoomInScreenshot;
@@ -80,8 +79,6 @@ typedef NS_ENUM(NSUInteger, MenuState)
 - (void)dismissGroupsMenu;
 - (void)launchPlaylist;
 - (void)dismissPlaylist;
-- (void)toggleOverlay:(UIGestureRecognizer *)gesture;
-- (void)toggleRadialMenu:(UIGestureRecognizer *)gesture;
 
 /// Transition Methods
 - (void)transformInAnimation;
@@ -112,7 +109,6 @@ typedef NS_ENUM(NSUInteger, MenuState)
         _groupTitle = title;
         _videoFrames = videoFrames;
         _menuState = MenuStateNone;
-        _radialMenuVisible = NO;
         
         id defaultTracker = [GAI sharedInstance].defaultTracker;
         [defaultTracker sendEventWithCategory:kGAICategoryBrowse
@@ -177,9 +173,6 @@ typedef NS_ENUM(NSUInteger, MenuState)
     [self.view setFrame:CGRectMake(0.0f, 0.0f, kShelbySPVideoWidth, kShelbySPVideoHeight)];
     [self.view setBackgroundColor:[UIColor blackColor]];
     [self setTrackedViewName:[NSString stringWithFormat:@"Playlist - %@", _groupTitle]];
-    
-    _radialMenu = [[ALRadialMenu alloc] init];
-    self.radialMenu.delegate = self;
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -291,9 +284,9 @@ typedef NS_ENUM(NSUInteger, MenuState)
     [self.overlayView.categoryTitleLabel setText:_groupTitle];
     [self.view addSubview:_overlayView];
     
-    _toggleOverlayGesuture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(toggleOverlay:)];
+    self.toggleOverlayGesuture = [[UITapGestureRecognizer alloc] initWithTarget:_overlayView action:@selector(toggleOverlay)];
     [self.toggleOverlayGesuture setNumberOfTapsRequired:1];
-    [self.view addGestureRecognizer:self.toggleOverlayGesuture];
+    [self.view addGestureRecognizer:_toggleOverlayGesuture];
     
     UIPinchGestureRecognizer *pinchOverlayGesuture = [[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(homeButtonAction:)];
     [self.view addGestureRecognizer:pinchOverlayGesuture];
@@ -726,10 +719,6 @@ typedef NS_ENUM(NSUInteger, MenuState)
 #pragma mark -  Update Methods (Private)
 - (void)currentVideoDidChangeToVideo:(NSUInteger)position
 {
-    
-    if (self.radialMenuVisible) {
-        [self toggleRadialMenu:nil];
-    }
     
     // Post notification (to rollViews that may have a keyboard loaded in view)
     [[NSNotificationCenter defaultCenter] postNotificationName:kShelbySPUserDidSwipeToNextVideo object:nil];
@@ -1176,21 +1165,10 @@ typedef NS_ENUM(NSUInteger, MenuState)
     [self.overlayView togglePlaylistView];
 }
 
-- (void)toggleOverlay:(UIGestureRecognizer *)gesture
-{
-    if ([self.overlayView isOverlayHidden] || self.radialMenuVisible) {
-        [self toggleRadialMenu:gesture];
-    }
-    [self.overlayView toggleOverlay];
-}
 
 - (void)toggleMenues:(UISwipeGestureRecognizer *)gesture
 {
     UISwipeGestureRecognizerDirection direction = [gesture direction];
- 
-    if (self.radialMenuVisible) {
-        [self toggleRadialMenu:gesture];
-    }
     
     if ([self.overlayView isOverlayHidden]) {
         [self.overlayView toggleOverlay];
@@ -1212,13 +1190,6 @@ typedef NS_ENUM(NSUInteger, MenuState)
         }
     }
 }
-
-- (void)toggleRadialMenu:(UIGestureRecognizer *)gesture
-{
-    [self.radialMenu buttonsWillAnimateFromButton:gesture inView:self.overlayView];
-    [self setRadialMenuVisible:!self.radialMenuVisible];
-}
-
 
 #pragma mark - Transition Methods (Private)
 - (void)transformInAnimation
@@ -1338,62 +1309,4 @@ typedef NS_ENUM(NSUInteger, MenuState)
     }
 }
 
-#pragma mark - ALRadial Methods
-- (NSInteger)numberOfItemsInRadialMenu:(ALRadialMenu *)radialMenu {
-	return 3;
-}
-
-
-- (NSInteger)arcSizeForRadialMenu:(ALRadialMenu *)radialMenu
-{
-    return 360;
-}
-
-
-- (NSInteger) arcRadiusForRadialMenu:(ALRadialMenu *)radialMenu
-{
-    return 72;
-}
-
-
-- (UIImage *)radialMenu:(ALRadialMenu *)radialMenu imageForIndex:(NSInteger) index {
-    
-    if (radialMenu == self.radialMenu) {
-        
-        if (index == 1) {
-			return [UIImage imageNamed:@"share"];
-		} else if (index == 2) {
-			return [UIImage imageNamed:@"like"];
-		} else if (index == 3) {
-			return [UIImage imageNamed:@"roll"];
-		}
-	}
-    
-	return nil;
-}
-
-
-- (void) radialMenu:(ALRadialMenu *)radialMenu didSelectItemAtIndex:(NSInteger)index
-{
-    [self.radialMenu itemsWillDisapearIntoButton:nil];
-    
-    
-    switch ( index ) {
-            
-        case 1:
-        {
-            break;
-        }
-        case 2:
-        {
-            break;
-        }
-        case 3:
-        {
-            break;
-        }
-        default:
-            break;
-    }
-}
 @end
