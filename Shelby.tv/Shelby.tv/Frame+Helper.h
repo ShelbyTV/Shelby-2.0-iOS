3b0b297756c013c4698e28022360ab2e4baf819f
Dan Spinosa <spinosa@gmail.com>
2013-04-25 19:15:51 -0400

    return nil b/c it's id, fix copyright, per @kepsolution

--- Shelby.tv/Shelby.tv/Frame+Helper.h
+++ Shelby.tv/Shelby.tv/Frame+Helper.h
@@ -3,7 +3,7 @@
 //  Shelby.tv
 //
 //  Created by Daniel Spinosa on 4/25/13.
-//  Copyright (c) 2013 Arthur Ariel Sabintsev. All rights reserved.
+//  Copyright (c) 2013 Shelby TV. All rights reserved.
 //
 
 #import "Frame.h"
