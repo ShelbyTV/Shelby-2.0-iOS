2824465f9f4a27036628210baa297bb9c3aa45c9
Dan Spinosa <spinosa@gmail.com>
2013-05-30 18:57:00 -0400

    send correct fetch entries error delegate calls

--- Shelby.tv/Shelby.tv/ShelbyDataMediator.m
+++ Shelby.tv/Shelby.tv/ShelbyDataMediator.m
@@ -216,7 +216,8 @@ NSString * const kShelbyOfflineLikesID = @"kShelbyOfflineLikesID";
                 });
                 
             } else {
-                [self.delegate fetchChannelsDidCompleteWithError:error];
+                [self.delegate fetchEntriesDidCompleteForChannel:(DisplayChannel *)[[self mainThreadContext] objectWithID:channel.objectID]
+                                                       withError:error];
             }
                                      
     }];
@@ -279,7 +280,8 @@ NSString * const kShelbyOfflineLikesID = @"kShelbyOfflineLikesID";
                 });
             });
         } else {
-            [self.delegate fetchChannelsDidCompleteWithError:error];
+            [self.delegate fetchEntriesDidCompleteForChannel:(DisplayChannel *)[[self mainThreadContext] objectWithID:channel.objectID]
+                                                   withError:error];
         }
     }];
 }
