ddd0e2e32b7ddbc05340f01727d9de36118037ef
Keren Pinkas <keren@shelby.tv>
2013-07-01 18:51:12 -0400

    Code for layouts for detail view - and added one more outlet

--- Shelby.tv/Shelby.tv/StreamBrowseCellForegroundView.m
+++ Shelby.tv/Shelby.tv/StreamBrowseCellForegroundView.m
@@ -8,6 +8,8 @@
 
 #import "StreamBrowseCellForegroundView.h"
 
+#define kShelbyInfoViewMargin 15
+
 @interface  StreamBrowseCellForegroundView()
 @property (nonatomic, weak) IBOutlet UIImageView *summaryUserAvatar;
 @property (weak, nonatomic) IBOutlet UILabel *summaryUsername;
@@ -16,6 +18,7 @@
 @property (nonatomic, weak) IBOutlet UIImageView *detailUserAvatar;
 @property (weak, nonatomic) IBOutlet UILabel *detailUsername;
 @property (weak, nonatomic) IBOutlet UILabel *detailViaNetwork;
+@property (weak, nonatomic) IBOutlet UIView *detailUserView;
 @property (weak, nonatomic) IBOutlet UIView *detailCommentView;
 @property (weak, nonatomic) IBOutlet UIView *detailNetworkShares;
 @end
@@ -30,6 +33,30 @@
     }
     return self;
 }
+
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+ 
+    NSInteger pageWidth = self.frame.size.width / 2;
+//    NSInteger pageHeight = self.frame.size.height;
+    NSInteger xOrigin = pageWidth + kShelbyInfoViewMargin;
+  
+    if (UIInterfaceOrientationIsLandscape([[UIApplication sharedApplication] statusBarOrientation])) {
+        // Landscape
+        self.detailTitle.frame = CGRectMake(xOrigin, 60, 280, 17);
+        self.detailUserView.frame = CGRectMake(xOrigin, 90, 160, 60);
+        self.detailCommentView.frame = CGRectMake(xOrigin, 160, pageWidth - kShelbyInfoViewMargin * 2, 60);
+        self.detailNetworkShares.frame = CGRectMake(xOrigin + self.detailUserView.frame.size.width + kShelbyInfoViewMargin, 100, 280, 40);
+    } else {
+        // Portrait
+        self.detailTitle.frame = CGRectMake(xOrigin, 60, 280, 17);
+        self.detailUserView.frame = CGRectMake(xOrigin, 90, 160, 60);
+        self.detailCommentView.frame = CGRectMake(xOrigin, 160, 280, 100);
+        self.detailNetworkShares.frame = CGRectMake(xOrigin, 270, 280, 40);
+    }
+}
+
 
 /*
 // Only override drawRect: if you perform custom drawing.
