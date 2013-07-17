fbf81312b1093748a6e3ff8fb5b9ff233fccd072
Dan Spinosa <spinosa@gmail.com>
2013-07-17 18:25:20 -0400

    welcome flow content views with parallax background

--- Shelby.tv/Shelby.tv/WelcomeFlowViewController.h
+++ Shelby.tv/Shelby.tv/WelcomeFlowViewController.h
@@ -7,6 +7,7 @@
 //
 
 #import "ShelbyViewController.h"
+#import "STVParallaxView.h"
 
 @class WelcomeFlowViewController;
 
--- Shelby.tv/Shelby.tv/WelcomeFlowViewController.m
+++ Shelby.tv/Shelby.tv/WelcomeFlowViewController.m
@@ -17,7 +17,12 @@ typedef NS_ENUM(NSInteger, ShelbyWelcomeFlowStatus)
 };
 
 @interface WelcomeFlowViewController ()
-
+@property (strong, nonatomic) STVParallaxView *parallaxView;
+@property (strong, nonatomic) IBOutlet UIView *page1;
+@property (strong, nonatomic) IBOutlet UIView *page2;
+@property (strong, nonatomic) IBOutlet UIView *page3;
+@property (strong, nonatomic) IBOutlet UIView *page4;
+@property (strong, nonatomic) IBOutlet UIView *background;
 @end
 
 @implementation WelcomeFlowViewController
@@ -26,7 +31,7 @@ typedef NS_ENUM(NSInteger, ShelbyWelcomeFlowStatus)
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
+        
     }
     return self;
 }
@@ -34,13 +39,39 @@ typedef NS_ENUM(NSInteger, ShelbyWelcomeFlowStatus)
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+
+    // 1) Parallax view: content
+    //strong pointers are wired up to all the views in this xib, no need to retain this array
+    [[NSBundle mainBundle] loadNibNamed:@"WelcomeFlowViews" owner:self options:nil];
+    UIView *parallaxForegroundView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 4*self.view.frame.size.width, self.view.frame.size.height)];
+    self.page1.frame = CGRectMake(0*self.view.frame.size.width, 0, self.view.frame.size.width, self.view.frame.size.height);
+    [parallaxForegroundView addSubview:self.page1];
+    self.page2.frame = CGRectMake(1*self.view.frame.size.width, 0, self.view.frame.size.width, self.view.frame.size.height);
+    [parallaxForegroundView addSubview:self.page2];
+    self.page3.frame = CGRectMake(2*self.view.frame.size.width, 0, self.view.frame.size.width, self.view.frame.size.height);
+    [parallaxForegroundView addSubview:self.page3];
+    self.page4.frame = CGRectMake(3*self.view.frame.size.width, 0, self.view.frame.size.width, self.view.frame.size.height);
+    [parallaxForegroundView addSubview:self.page4];
+
+    // 2) Parallax view: configuration
+    self.parallaxView = [[STVParallaxView alloc] initWithFrame:self.view.frame];
+    //move background left a bit to account for pulling
+    self.background.frame = CGRectMake(-100, 0, self.background.frame.size.width, self.background.frame.size.height);
+    self.parallaxView.backgroundContent = self.background;
+    self.parallaxView.foregroundContent = parallaxForegroundView;
+    self.parallaxView.parallaxRatio = 0.5;
+
+    [self.view addSubview:self.parallaxView];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+- (BOOL)shouldAutorotate
+{
+    return NO;
 }
 
 + (bool)isWelcomeComplete
@@ -48,12 +79,11 @@ typedef NS_ENUM(NSInteger, ShelbyWelcomeFlowStatus)
     return [[NSUserDefaults standardUserDefaults] integerForKey:kShelbyWelcomeFlowStatusKey] == ShelbyWelcomeFlowStatusComplete;
 }
 
-- (IBAction)completeTestTapped:(id)sender {
+- (IBAction)previewWasTapped:(id)sender {
     //TODO: enable saving status
-//    [[NSUserDefaults standardUserDefaults] setInteger:ShelbyWelcomeFlowStatusComplete forKey:kShelbyWelcomeFlowStatusKey];
-//    [[NSUserDefaults standardUserDefaults] synchronize]
+    //    [[NSUserDefaults standardUserDefaults] setInteger:ShelbyWelcomeFlowStatusComplete forKey:kShelbyWelcomeFlowStatusKey];
+    //    [[NSUserDefaults standardUserDefaults] synchronize]
     [self.delegate welcomeFlowDidTapPreview:self];
 }
-
 
 @end
