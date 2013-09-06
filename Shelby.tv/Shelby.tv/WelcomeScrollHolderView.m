2952b3db499125911f2140a8f23c132093b5d65b
Dan Spinosa <spinosa@gmail.com>
2013-09-06 18:40:44 -0400

    start of new welcome UI (horizontal phone, movie, zooms)

--- Shelby.tv/Shelby.tv/WelcomeScrollHolderView.m
+++ Shelby.tv/Shelby.tv/WelcomeScrollHolderView.m
@@ -7,14 +7,16 @@
 //
 
 #import "WelcomeScrollHolderView.h"
+#import <AVFoundation/AVFoundation.h>
+#import <MediaPlayer/MediaPlayer.h>
 
-@interface WelcomeScrollHolderView()
+@interface WelcomeScrollHolderView() {
+    MPMoviePlayerController *_player;
+}
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UIView *phoneView;
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
-
-@property (weak, nonatomic) IBOutlet UIImageView *tip1;
-@property (weak, nonatomic) IBOutlet UIImageView *tip2;
-@property (weak, nonatomic) IBOutlet UIImageView *tip3;
 
 @end
 
@@ -33,19 +35,20 @@
 {
     self.scrollView.delegate = self;
 
-    UIView *page0 = [self pageForImageNamed:@"welcome-page-0" atIndex:0];
-    [self.scrollView addSubview:page0];
-    UIView *page1 = [self pageForImageNamed:@"welcome-page-1" atIndex:1];
-    [self.scrollView addSubview:page1];
-    UIView *page2 = [self pageForImageNamed:@"welcome-page-2" atIndex:2];
-    [self.scrollView addSubview:page2];
+    //page 0 is movie
+    [self initPlayerWithMovie:@"hungry" atIndex:0];
+    [self.scrollView addSubview:_player.view];
+    [_player play];
 
-    //TODO: 3a and 3b go into their own little scroller, so they can slide left/right
-    UIView *page3a = [self pageForImageNamed:@"welcome-page-3a" atIndex:3];
+    UIView *page1 = [self pageForImageNamed:@"welcome-h-p1" atIndex:1];
+    [self.scrollView addSubview:page1];
+    UIView *page2 = [self pageForImageNamed:@"welcome-h-p2" atIndex:2];
+    [self.scrollView addSubview:page2];
+    UIView *page3a = [self pageForImageNamed:@"welcome-h-p3-bg" atIndex:3];
     [self.scrollView addSubview:page3a];
-//    UIView *page3b = [self pageForImageNamed:@"welcome-page-3b" atIndex:3];
-//    [self.scrollView addSubview:page3b];
     self.scrollView.contentSize = CGSizeMake(self.scrollView.bounds.size.width, self.scrollView.bounds.size.height * PAGES_IN_SCROLL_VIEW);
+
+    //TODO: add p3 summary + detail overlays
 }
 
 #pragma mark - UIScrollViewDelegate
@@ -80,25 +83,26 @@
 
 - (void)showTip:(NSUInteger)tipIdx
 {
-    for (UIImageView *t in @[self.tip1, self.tip2, self.tip3]) {
-        t.hidden = YES;
-    }
-
     switch (tipIdx) {
         case 0:
-            self.titleLabel.text = @"Discover & enjoy the best video.";
+            [self zoomOutOnPhone];
+            self.titleLabel.text = @"Shelby brings you 15 minutes of video everyday.";
+            self.tipLabel.text = @"";
             break;
         case 1:
-            self.titleLabel.text = @"Your perfect video stream.";
-            self.tip1.hidden = NO;
+            [self zoomInOnPhone];
+            self.titleLabel.text = @"...from your favorite people and places.";
+            self.tipLabel.text = @"Shelby users share great new video all day long";
             break;
         case 2:
-            self.titleLabel.text = @"Shelby gets to know you.";
-            self.tip2.hidden = NO;
+            [self zoomInOnPhone];
+            self.titleLabel.text = @"It's like a TV channel personalized for you.";
+            self.tipLabel.text = @"Like and share videos to get better recommendations.";
             break;
         case 3:
-            self.titleLabel.text = @"Shelby is powered by friends.";
-            self.tip3.hidden = NO;
+            [self zoomInOnPhone];
+            self.titleLabel.text = @"...a TV channel powered by your friends.";
+            self.tipLabel.text = @"Swipe left to see what your friend said.";
             break;
         default:
             break;
@@ -111,6 +115,39 @@
     UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(scrollPageBounds.origin.x, idx*scrollPageBounds.size.height, scrollPageBounds.size.width, scrollPageBounds.size.height)];
     imageView.image = [UIImage imageNamed:imageName];
     return imageView;
+}
+
+- (void)initPlayerWithMovie:(NSString *)movieName atIndex:(NSUInteger)idx
+{
+    CGRect scrollPageBounds = self.scrollView.bounds;
+
+    NSURL *vidURL = [[NSBundle mainBundle] URLForResource:movieName withExtension:@"m4v"];
+
+    _player = [[MPMoviePlayerController alloc] initWithContentURL:vidURL];
+    _player.view.frame = CGRectMake(scrollPageBounds.origin.x, idx*scrollPageBounds.size.height, scrollPageBounds.size.width, scrollPageBounds.size.height);
+    _player.repeatMode = MPMovieRepeatModeOne;
+    _player.controlStyle = MPMovieControlStyleNone;
+    [_player play];
+}
+
+- (void)zoomOutOnPhone
+{
+    CGFloat s = .83;
+    CGAffineTransform transform = CGAffineTransformScale(CGAffineTransformIdentity, s, s);
+    [UIView animateWithDuration:.5 animations:^{
+        self.phoneView.transform = transform;
+    } completion:^(BOOL finished) {
+        //
+    }];
+}
+
+- (void)zoomInOnPhone
+{
+    [UIView animateWithDuration:.5 animations:^{
+        self.phoneView.transform = CGAffineTransformIdentity;
+    } completion:^(BOOL finished) {
+        //
+    }];
 }
 
 @end
