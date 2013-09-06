9039be457ce05b2f6b4383d322db6842b2716714
Dan Spinosa <spinosa@gmail.com>
2013-09-06 14:41:32 -0400

    placeholder text for share view

---
+++ Shelby.tv/Shelby.tv/UITextViewWithPlaceholder.h
@@ -0,0 +1,16 @@
+//
+//  UITextViewWithPlaceholder.h
+//  Shelby.tv
+//
+//  Created by Daniel Spinosa on 9/6/13.
+//  Copyright (c) 2013 Shelby TV, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITextViewWithPlaceholder : UITextView
+
+@property (nonatomic, strong) NSString *placeholder;
+@property (nonatomic, strong) UIColor *placeholderColor;
+
+@end
