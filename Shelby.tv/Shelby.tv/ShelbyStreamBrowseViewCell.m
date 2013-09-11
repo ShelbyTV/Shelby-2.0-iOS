5605e4205198715899b585c6eb0398f6acca9794
Dan Spinosa <spinosa@gmail.com>
2013-09-11 13:05:15 -0400

    remove unused and commented-out code

--- Shelby.tv/Shelby.tv/ShelbyStreamBrowseViewCell.m
+++ Shelby.tv/Shelby.tv/ShelbyStreamBrowseViewCell.m
@@ -212,26 +212,7 @@
         //regular background
         self.thumbnailRegularView.image = image;
 
-        //XXX No longer using blurred background in iOS6 or 7
-        //      but leaving this code here until that decision has time to sink in (ds 8/13/13)
-        //blurred background
-        // The one thing that seems slow is blurring very high resolution images...
-        //OPTIMIZE: No need to run on every pixel of a huge image, just to make it blurry.
-        //          Run the filter over a smaller image, see how much faster that gets.
-//        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-//            CIImage *inputImage = [CIImage imageWithCGImage:image.CGImage];
-//            [self.blurFilter setValue:inputImage forKey:@"inputImage"];
-//            CIImage *result = [self.blurFilter valueForKey:@"outputImage"];
-//            //use inputImage's size to account for gaussian blur shrinkage
-//            CGImageRef cgImage = [self.ciContext createCGImage:result fromRect:[inputImage extent]];
-//            dispatch_async(dispatch_get_main_queue(), ^{
-//                [UIView transitionWithView:self.thumbnailBlurredView duration:0.2 options:UIViewAnimationOptionTransitionCrossDissolve animations:^{
-//                    self.thumbnailBlurredView.image = [UIImage imageWithCGImage:cgImage];
-//                } completion:nil];
-//                CFRelease(cgImage);
-//                [self.blurFilter setValue:nil forKey:@"inputImage"];
-//            });
-//        });
+        //No longer using blurred (or any) background in iOS6 or 7
     }
 }
 
