4c4b00e93592b54bc5502419b3da83742788702a
Keren Pinkas <keren@shelby.tv>
2013-12-23 10:56:12 -0500

    Added remote notifications to Nightly target. And empty AppDelegate methods

--- Shelby.tv/Shelby.tv/AppDelegate.m
+++ Shelby.tv/Shelby.tv/AppDelegate.m
@@ -298,4 +298,26 @@
     }
 }
 
+#pragma mark - Push Notifications
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+    
+}
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
+    
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+{
+    
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult result))completionHandler
+
+{
+    
+}
+
+
 @end
