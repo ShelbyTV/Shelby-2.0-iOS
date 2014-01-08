b38adba1525a83e5bd737b4dfaa55d54c82182af
Keren Pinkas <keren@shelby.tv>
2014-01-08 14:47:29 -0500

    No need for Shadow in iOS7

--- Shelby.tv/Shelby.tv/AppDelegate.m
+++ Shelby.tv/Shelby.tv/AppDelegate.m
@@ -226,7 +226,7 @@
 {
     //navigation bars
     [[UINavigationBar appearance] setBackgroundImage:[UIImage imageNamed:@"top-nav-bkgd"] forBarMetrics:UIBarMetricsDefault];
-    [[UINavigationBar appearance] setTitleTextAttributes:@{NSFontAttributeName:[UIFont fontWithName:@"Ubuntu-Medium" size:20.0], NSForegroundColorAttributeName: [UIColor whiteColor], NSShadowAttributeName: [UIColor clearColor]}];
+    [[UINavigationBar appearance] setTitleTextAttributes:@{NSFontAttributeName:[UIFont fontWithName:@"Ubuntu-Medium" size:20.0], NSForegroundColorAttributeName: [UIColor whiteColor]}];
 }
 
 - (void)setupCrashHandling
