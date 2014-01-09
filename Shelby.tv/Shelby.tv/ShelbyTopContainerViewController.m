ddb1c18696f8773f5bded9dae07bb76ccc973260
spinosa <spinosa@gmail.com>
2014-01-09 17:53:17 -0500

    outlets for ShelbyTopContainerView’s containers; ability to toggle fullscreen

--- Shelby.tv/Shelby.tv/ShelbyTopContainerViewController.m
+++ Shelby.tv/Shelby.tv/ShelbyTopContainerViewController.m
@@ -11,11 +11,19 @@
 #import "ShelbyVideoReelViewController.h"
 
 @interface ShelbyTopContainerViewController ()
+//container Views
+@property (weak, nonatomic) IBOutlet UIView *navigationViewContainer;
+@property (weak, nonatomic) IBOutlet UIView *currentlyPlayingViewContainer;
+@property (weak, nonatomic) IBOutlet UIView *videoReelViewContainer;
+
+//View Controllers
 @property (nonatomic, strong) ShelbyVideoReelViewController *videoReelVC;
 @property (nonatomic, strong) ShelbyNavigationViewController *sideNavigation;
 @end
 
-@implementation ShelbyTopContainerViewController
+@implementation ShelbyTopContainerViewController {
+    CGRect _fullscreenVideoFrame, _smallscreenVideoFrame;
+}
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -30,6 +38,37 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    
+    _fullscreenVideoFrame = CGRectMake(0, 0, 1024, 768);
+    _smallscreenVideoFrame = self.videoReelViewContainer.frame;
+    
+    //Uncomment for some fun layout testing
+//    [NSTimer scheduledTimerWithTimeInterval:3.0f target:self selector:@selector(toggleFullscreenVideo) userInfo:nil repeats:YES];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)toggleFullscreenVideo
+{
+    CGRect newFrame;
+    if ([self isVideoFullscreen]) {
+        newFrame = _smallscreenVideoFrame;
+    } else {
+        newFrame = _fullscreenVideoFrame;
+    }
+
+    [UIView animateWithDuration:.5 delay:0 usingSpringWithDamping:.8 initialSpringVelocity:12.f options:UIViewAnimationOptionCurveEaseIn animations:^{
+        self.videoReelViewContainer.frame = newFrame;
+    } completion:nil];
+}
+
+- (BOOL)isVideoFullscreen
+{
+    return CGRectEqualToRect(self.videoReelViewContainer.frame, _fullscreenVideoFrame);
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
@@ -47,12 +86,6 @@
         self.sideNavigation.currentUser = self.currentUser;
         self.sideNavigation.videoReelVC = self.videoReelVC;
     }
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
