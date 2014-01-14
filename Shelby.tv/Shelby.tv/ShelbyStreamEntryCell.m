a920ce63556c1aa2fb6bbd48a95e40f1ac2efbdc
Keren Pinkas <keren@shelby.tv>
2014-01-14 15:13:49 -0500

    Add likers view.

--- Shelby.tv/Shelby.tv/ShelbyStreamEntryCell.m
+++ Shelby.tv/Shelby.tv/ShelbyStreamEntryCell.m
@@ -17,6 +17,9 @@
 @property (nonatomic, weak) IBOutlet UIImageView *videoThumbnail;
 @property (nonatomic, weak) IBOutlet UIImageView *userAvatar;
 @property (nonatomic, weak) IBOutlet UILabel *description;
+@property (nonatomic, weak) IBOutlet UIView *likersView;
+@property (nonatomic, strong) NSMutableArray *likerImageViews;
+@property (nonatomic, strong) NSMutableOrderedSet *likers;
 
 - (IBAction)shareVideo:(id)sender;
 - (IBAction)toggleLike:(id)sender;
@@ -34,6 +37,24 @@
 }
 
 
+- (void)prepareForReuse
+{
+    NSMutableArray *likerViews = [@[] mutableCopy];
+    CGFloat likerX = 0.f;
+    CGFloat likerSharerHeight = 30.f;
+    UIImageView *likerImageView;
+    for (int i = 0; i < 6; i++) {
+        likerImageView = [[UIImageView alloc] initWithFrame:CGRectMake(likerX, 5, likerSharerHeight, likerSharerHeight)];
+        [self.likersView addSubview:likerImageView];
+        likerImageView.layer.cornerRadius = 3.0f;
+        likerImageView.layer.masksToBounds = YES;
+        [likerViews addObject:likerImageView];
+        likerX += likerSharerHeight + 10;
+    }
+    
+    self.likerImageViews = likerViews;
+}
+
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated
 {
     [super setSelected:selected animated:animated];
@@ -44,6 +65,15 @@
 - (void)setVideoFrame:(Frame *)videoFrame
 {
     _videoFrame = videoFrame;
+
+    // KP KP - need to fetch upvoters from backend like we do on iphone
+    // KP KP TODO- add/remove observer
+//    [self.videoFrame addObserver:self forKeyPath:@"upvoters" options:NSKeyValueObservingOptionNew context:nil];
+    if ([self.videoFrame.upvoters count]) {
+        [self processLikersAndSharers];
+        
+        [self updateLikersAndSharersVisuals];
+    }
     
     self.videoTitle.text = self.videoFrame.video.title;
     NSString *captionText = [videoFrame creatorsInitialCommentWithFallback:YES];
@@ -63,7 +93,51 @@
     } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error) {
         
     }];
+}
+
+//- (void)observeValueForKeyPath:(NSString *)keyPath
+//                      ofObject:(id)object
+//                        change:(NSDictionary *)change
+//                       context:(void *)context
+//{
+//    [self processLikersAndSharers];
+//    
+//    [self updateLikersAndSharersVisuals];
+//}
+
+- (void)processLikersAndSharers
+{
+    if (!self.likers) {
+        self.likers = [NSMutableOrderedSet new];
+    } else {
+        [self.likers removeAllObjects];
+    }
     
+    for (User *liker in self.videoFrame.upvoters) {
+        [self.likers addObject:liker];
+    }
+}
+
+- (void)updateLikersAndSharersVisuals
+{
+    for (UIImageView *iv in self.likerImageViews) {
+        iv.image = nil;
+    }
+    
+    if ([self.likers count]) {
+        //        self.detailNoLikersLabel.hidden = YES;
+        for (NSUInteger i = 0; i < MIN([self.likers count], [self.likerImageViews count]); i++) {
+            User *liker = self.likers[i];
+            [((UIImageView *)self.likerImageViews[i]) setImageWithURL:liker.avatarURL placeholderImage:[UIImage imageNamed:@"avatar-blank"]];
+        }
+    } else if ([self.videoFrame.video.trackedLikerCount intValue]) {
+        //        self.detailNoLikersLabel.hidden = NO;
+        //        self.detailNoLikersLabel.text = @"See all who liked this...";
+    } else {
+        //        self.detailNoLikersLabel.hidden = NO;
+        //        self.detailNoLikersLabel.text = @"Be the first to like this!";
+    }
+
 }
 
 - (IBAction)shareVideo:(id)sender
