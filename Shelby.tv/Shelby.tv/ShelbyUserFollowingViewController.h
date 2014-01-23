d7738772d07eae0c5fc1b607e5a84ee0065fa2d5
spinosa <spinosa@gmail.com>
2014-01-23 17:13:40 -0500

    fix layout in UserInfoVC; framework for switching between Activity and Following in UserInfoVC; structure of UserFollowingVC

---
+++ Shelby.tv/Shelby.tv/ShelbyUserFollowingViewController.h
@@ -0,0 +1,16 @@
+//
+//  ShelbyUserFollowingViewController.h
+//  Shelby.tv
+//
+//  Created by Dan Spinosa on 1/23/14.
+//  Copyright (c) 2014 Shelby TV, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "User.h"
+
+@interface ShelbyUserFollowingViewController : UITableViewController
+
+@property (strong, nonatomic) User *user;
+
+@end
