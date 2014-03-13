953f7f0f351d811ffe6d3321b39ef8ca23621b7f
spinosa <spinosa@gmail.com>
2014-03-13 13:38:47 -0400

    correct ARC property init

--- Shelby.tv/Shelby.tv/ShelbyStreamBrowseViewController.h
+++ Shelby.tv/Shelby.tv/ShelbyStreamBrowseViewController.h
@@ -82,7 +82,7 @@ maintainingCurrentFocus:(BOOL)shouldMaintainCurrentFocus;
 @property (nonatomic, assign) NSUInteger currentPage;
 
 // To allow our superview to adjust visual stylings
-@property (readonly) UICollectionView *collectionView;
+@property (readonly, weak) UICollectionView *collectionView;
 @property (nonatomic, readonly) BOOL hasNoContent;
 
 @end
